3bd765ea0dcd38f44a4147a813732590af6f5684
hizhnyack <khizhnyak.yp@gmail.com>
2024-12-04 19:54:25 +0300

    update

--- self_howitworks.py
+++ self_howitworks.py
@@ -2,15 +2,24 @@ from symbol import star_expr
 
 
 class human():
+
     def __init__(self,age=0,gender='?'):
         self.age = age
         self.gender = gender
+
     def speak(self):
         print('I am a', self.gender, 'and I', self.age,'years old')
 
+    def change(self,person):
+        temp = []
+        self = temp
+        self = person
+        person = temp
+        return self, person
 
-Jane = human(24, 'woman')
-John = human(22, 'man')
+
+Jane = human(24, 'female')
+John = human(22, 'male')
 
 # Jane.speak()
 # John.speak()
--- test.py
+++ test.py
@@ -4,6 +4,12 @@ Ann = human(28,'female')
 Igor = human(56,'male')
 
 Ann.speak()
-Igor.speak()
-
+# Igor.speak()
 Jane.speak()
+
+Ann.change(Jane)
+
+Ann.speak()
+# Igor.speak()
+Jane.speak()
+
